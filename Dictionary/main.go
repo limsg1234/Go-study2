2803e1ed18a1fa8d074c263a5550ff96ff134253
sglim <53066410+limsg1234@users.noreply.github.com>
2021-11-08 00:00:59 +0900

    문법 연습2

---
+++ Dictionary/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"nomad/Dictionary/mydict"
+)
+
+func main() {
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(word)
+	}
+}
